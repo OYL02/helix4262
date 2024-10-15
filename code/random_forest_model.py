da0ad7d78a17faeb8c8239a9f12e016b3cfcfa0a
tzexinl <tzexin.lim@u.nus.edu>
2024-10-15 20:22:15 +0800

    Updated cleaned up codes

--- code/random_forest_model.py
+++ code/random_forest_model.py
@@ -1,17 +1,17 @@
 # 1. Importing relevant modules
 import pandas as pd
-import numpy as np
-import random
 import joblib 
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV 
+from scipy.stats import randint
 
 
 # 2. Importing training sets 
 X_train = pd.read_csv()
 y_train = pd.read_csv()
+y_train = y_train['label']
 
 
 # 3. Creation of model
@@ -19,14 +19,15 @@ y_train = pd.read_csv()
 rf = RandomForestClassifier()
 
 # With hyperparameter tuning: Use random search to find the best hyperparameters
-param_dist = {'n_estimators': random.randint(50, 500),
-              'max_depth': random.randint(1, 20)}
+param_dist = {'n_estimators': randint(50, 500),
+              'max_depth': randint(1, 20)}
 random_search = RandomizedSearchCV(rf,
                                    param_distributions = param_dist, 
                                    n_iter = 10, 
                                    cv = 10)
 #print('Best hyperparameters:', random_search.best_params_)
 random_search.fit(X_train, y_train)
+# rf.fit(X_train, y_train)
 
 
 # 4. Save the trained model 
--- code/xg_boost_model.py
+++ code/xg_boost_model.py
@@ -1,24 +1,24 @@
 # 1. Importing relevant modules
 import pandas as pd
-import numpy as np
-import random
 import joblib 
 
 from xgboost import XGBClassifier
 from sklearn.model_selection import RandomizedSearchCV 
+from scipy.stats import randint
 
 
 # 2. Importing training sets 
 X_train = pd.read_csv()
 y_train = pd.read_csv()
+y_train = y_train['label']
 
 
 # 3. Creation of model
 xgboost = XGBClassifier()
 
 # With hyperparameter tuning: Use random search to find the best hyperparameters
-param_dist = {'n_estimators': random.randint(50, 500),
-              'max_depth': random.randint(1, 20)}
+param_dist = {'n_estimators': randint(50, 500),
+              'max_depth': randint(1, 20)}
 random_search = RandomizedSearchCV(xgboost,
                                    param_distributions = param_dist, 
                                    n_iter = 10, 
