6f830265f9ba3b7b2bd11232e3ab738fee3ba61b
yilin <ong.yi.lin.23@gmail.com>
2024-10-31 18:33:00 +0800

    added class NewRNANanoporeDataset for non labelled datasets to do predictions on

--- code/neural_net_preproc.py
+++ code/neural_net_preproc.py
@@ -8,13 +8,13 @@ from torch.utils.data import Dataset
 # Add the parent directory to sys.path
 sys.path.append(str(Path(__file__).resolve().parent))
 
-from feature_eng_pipeline import pipeline_nn
+from feature_eng_pipeline import pipeline_nn, process_new_data
 
 
 class RNANanoporeDataset(Dataset):
     """Dataset used to train and test RNA Nanopore data"""
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, v_outpath, s_outpath):
         """Initializes instance of class RNANanoporeDataset.
 
         Args:
@@ -22,7 +22,7 @@ class RNANanoporeDataset(Dataset):
         """
 
         self.df = pd.read_csv(csv_file)
-        v, s, X_df, y_df = pipeline_nn(self.df)
+        v, s, X_df, y_df = pipeline_nn(self.df, v_outpath, s_outpath)
         #X_drop = X_df.drop(["transcript_name", "gene_id", "nucleotide_seq"], axis=1).reset_index(drop=True)  
 
         # keep all trigram columns in input dataset
@@ -51,3 +51,42 @@ class RNANanoporeDataset(Dataset):
     
     def get_size(self):
         return len(self)
+
+class NewRNANanoporeDataset(Dataset):
+    """Dataset to predict new (non training) RNA Nanopore data"""
+
+    def __init__(self, df, vectorizer, standardizer):
+        """
+        Initializes instance of class NewRNANanoporeDataset.
+
+        Args:
+            df (DataFrame): dataframe containing nanopore data
+            vectorizer: trained on training data, to transform new data
+            standardizer: trained on training data, to transform new data
+        """
+        
+        self.df = df
+        X_df = process_new_data(df, vectorizer, standardizer) 
+
+        # keep all trigram columns in input dataset
+        self.X = X_df
+
+    def __len__(self):
+        """Returns the size of the dataset"""
+        return len(self.X)
+
+    def __getitem__(self, idx):
+    # Handle if idx is a tensor (converting to list if needed)
+        if isinstance(idx, torch.Tensor):
+            idx = idx.tolist()
+
+        signal_features = self.X.iloc[idx].values  
+
+        # Convert to tensors
+        signal_features = torch.tensor(signal_features, dtype=torch.float32)
+
+        return signal_features
+    
+    def get_size(self):
+        return len(self)
+
